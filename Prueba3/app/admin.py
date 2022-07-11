450ad2ce167f8a26748dde96450c182138a5f45c
Bastián Díaz <102882388+bastidiazp@users.noreply.github.com>
2022-07-11 14:59:11 -0400

    Delete Prueba3 directory

--- Prueba3/app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Tipo, Producto, Formulario
-
-# Register your models here.
-admin.site.register(Tipo)
-admin.site.register(Producto)
-admin.site.register(Formulario)
